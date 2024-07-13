9eac1ece265d7fad1d0a047521d6146c3e2d13f0
Kristof Ulicsny <ulicsny.kristof@gmail.com>
2024-07-13 10:50:39 +0200

    Aktivitás link added on navbar

--- ajandek_eger/src/navigation-bar/TopNavigationBar.tsx
+++ ajandek_eger/src/navigation-bar/TopNavigationBar.tsx
@@ -2,7 +2,6 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 
 function TopNavigationBar() {
   return (
@@ -16,19 +15,7 @@ function TopNavigationBar() {
               <Nav.Link href="/termekek">Termékek</Nav.Link>
               <Nav.Link href="/adminisztracio">Adminisztráció</Nav.Link>
               <Nav.Link href="/statisztikak">Statisztikák</Nav.Link>
-              <NavDropdown title="Egyéb" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link href="/statisztikak">Aktivitás</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
