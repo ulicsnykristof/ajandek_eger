a6ad930cded587df4f1906399d61c93f9a6e0b38
Kristof Ulicsny <ulicsny.kristof@gmail.com>
2024-07-15 15:46:54 +0200

    visual changes

--- ajandek_eger/src/components/termekek-page/termek-page/Termek.tsx
+++ ajandek_eger/src/components/termekek-page/termek-page/Termek.tsx
@@ -24,101 +24,123 @@ function Termek() {
   };
   return (
     <>
-      <h4>Termék adatok</h4>
+      <h4 className="termek-details-header">Termék adatok</h4>
       {termek.map((t) => (
-        <div key={t.cikkszam} style={{ marginLeft: "20px" }}>
-          <br />
-          <br />
-          <p>Cikkszam: </p>
-          <input
-            type="text"
-            name="cikkszam"
-            onChange={handleChange}
-            value={t.cikkszam}
-            style={{ width: "400px" }}
-          />
-          <br />
-          <br />
-          <p>Vonalkód: </p>
-          <input
-            type="text"
-            name="vonalkod"
-            onChange={handleChange}
-            value={t.vonalkod}
-            style={{ width: "400px" }}
-          />
-          <br />
-          <br />
-          <p>Név: </p>
-          <input
-            type="text"
-            name="nev"
-            onChange={handleChange}
-            value={t.nev}
-            style={{ width: "400px" }}
-          />
-          <br />
-          <br />
-          <p>Nettó eladási ár: </p>
-          <input
-            type="text"
-            name="eladarnetto"
-            onChange={handleChange}
-            value={t.eladarnetto}
-            style={{ width: "400px" }}
-          />
-          <br />
-          <br />
-          <p>Bruttó eladási ár: </p>
-          <input
-            type="text"
-            name="eladarbrutto"
-            onChange={handleChange}
-            value={t.eladarbrutto}
-            style={{ width: "400px" }}
-          />
-          <br />
-          <br />
-          <p>Darabszám: </p>
-          <input
-            type="text"
-            name="db"
-            onChange={handleChange}
-            value={t.db}
-            style={{ width: "400px" }}
-          />
-          <br />
-          <br />
-          <p>Tipus: </p>
-          <input
-            type="text"
-            name="tipus"
-            onChange={handleChange}
-            value={t.tipus}
-            style={{ width: "400px" }}
-          />
-          <br />
-          <br />
-          <p>Szín: </p>
-          <input
-            type="text"
-            name="szin"
-            onChange={handleChange}
-            value={t.szin}
-            style={{ width: "400px" }}
-          />
-          <br />
-          <br />
-          <p>Méret: </p>
-          <input
-            type="text"
-            name="meret"
-            onChange={handleChange}
-            value={t.meret}
-            style={{ width: "400px" }}
-          />
-          <br />
-          <br />
+        <div key={t.cikkszam} className="termek-details-main-div">
+          <div className="termek-details-div">
+            <div className="termek-details-p">
+              <p>Cikkszam: </p>
+            </div>
+            <div>
+              <input
+                className="termek-details-input"
+                type="text"
+                name="cikkszam"
+                onChange={handleChange}
+                value={t.cikkszam}
+              />
+            </div>
+          </div>
+          <div className="termek-details-div">
+            <div className="termek-details-p">
+              <p>Vonalkód: </p>
+            </div>
+            <input
+              className="termek-details-input"
+              type="text"
+              name="vonalkod"
+              onChange={handleChange}
+              value={t.vonalkod}
+            />
+          </div>
+          <div className="termek-details-div">
+            <div className="termek-details-p">
+              <p>Név: </p>
+            </div>
+            <input
+              className="termek-details-input"
+              type="text"
+              name="nev"
+              onChange={handleChange}
+              value={t.nev}
+            />
+          </div>
+          <div className="termek-details-div">
+            <div className="termek-details-p">
+              <p>Nettó eladási ár: </p>
+            </div>
+            <input
+              className="termek-details-input"
+              type="text"
+              name="eladarnetto"
+              onChange={handleChange}
+              value={t.eladarnetto}
+            />
+          </div>
+          <div className="termek-details-div">
+            <div className="termek-details-p">
+              <p>Bruttó eladási ár: </p>
+            </div>
+            <input
+              className="termek-details-input"
+              type="text"
+              name="eladarbrutto"
+              onChange={handleChange}
+              value={t.eladarbrutto}
+            />
+          </div>
+          <div className="termek-details-div">
+            <div className="termek-details-p">
+              <p>Darabszám: </p>
+            </div>
+            <input
+              className="termek-details-input"
+              type="text"
+              name="db"
+              onChange={handleChange}
+              value={t.db}
+            />
+          </div>
+          <div className="termek-details-div">
+            <div className="termek-details-p">
+              <p>Tipus: </p>
+            </div>
+            <input
+              className="termek-details-input"
+              type="text"
+              name="tipus"
+              onChange={handleChange}
+              value={t.tipus}
+            />
+          </div>
+          <div className="termek-details-div">
+            <div className="termek-details-p">
+              <p>Szín: </p>
+            </div>
+            <input
+              className="termek-details-input"
+              type="text"
+              name="szin"
+              onChange={handleChange}
+              value={t.szin}
+            />
+          </div>
+          <div className="termek-details-div">
+            <div className="termek-details-p">
+              <p>Méret: </p>
+            </div>
+            <input
+              className="termek-details-input"
+              type="text"
+              name="meret"
+              onChange={handleChange}
+              value={t.meret}
+            />
+          </div>
+          <div className="termek-details-button-div">
+            <button className="termek-details-button">Módosítás</button>
+            <button className="termek-details-button">Törlés</button>
+          </div>
         </div>
       ))}
     </>
