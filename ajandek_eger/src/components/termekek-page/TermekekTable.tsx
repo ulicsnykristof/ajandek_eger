551546ee720ea0f6ae3c307e82565f78c9707085
Kristof Ulicsny <ulicsny.kristof@gmail.com>
2024-07-15 11:07:52 +0200

    Added delete button feature

--- ajandek_eger/src/components/termekek-page/TermekekTable.tsx
+++ ajandek_eger/src/components/termekek-page/TermekekTable.tsx
@@ -6,6 +6,8 @@ import { MouseEvent } from "react";
 import GetImage from "./GetImage";
 
 function TermekekTable() {
+  // On 'szerkeszt' button click navigate to 'termekek' page
+
   const navigate = useNavigate();
 
   const handleClick = (e: MouseEvent<HTMLElement>) => {
@@ -15,6 +17,22 @@ function TermekekTable() {
       state: { param1: "1", param2: "2", param3: "3" },
     });
   };
+
+  // On 'törlés' button clicks, deletes the selected item
+
+  const handleClickDelete = async (e: MouseEvent<HTMLElement>) => {
+    const btnid = e.currentTarget.id;
+
+    try {
+      await axios.delete("http://localhost:8080/deleteTermek/" + btnid);
+    } catch (err) {}
+
+    try {
+      await axios.delete("http://localhost:8080/deleteImage/" + btnid);
+    } catch (err) {}
+  };
+
+  // Get all data from database to 'termekek' /////////////
 
   const [termekek, setTermekek] = useState<any[]>([]);
   useEffect(() => {
@@ -28,6 +46,8 @@ function TermekekTable() {
     };
     fetchAllPlanet();
   }, []);
+
+  // Pagination //////////////////////////////////////////
 
   const [currentPage, setCurrentPage] = useState(1);
   const [recordsPerPage] = useState(4);
@@ -110,7 +130,13 @@ function TermekekTable() {
                   >
                     Szerkesztés
                   </Button>
-                  <Button style={{ marginTop: "40px" }}>Törlés</Button>
+                  <Button
+                    id={termek.cikkszam}
+                    onClick={handleClickDelete}
+                    style={{ marginTop: "40px" }}
+                  >
+                    Törlés
+                  </Button>
                 </td>
               </tr>
             ))}
