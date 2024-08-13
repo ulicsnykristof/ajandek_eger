00b71d1ece60d8b99c939f3831aa14be0daba287
Kristof Ulicsny <ulicsny.kristof@gmail.com>
2024-08-13 13:30:10 +0200

    change picture fixed

--- ajandek_eger/src/components/termekek-page/termek-page/TermekImage.tsx
+++ ajandek_eger/src/components/termekek-page/termek-page/TermekImage.tsx
@@ -2,6 +2,7 @@ import { useSearchParams } from "react-router-dom";
 import GetImage from "../GetImage";
 import { useState } from "react";
 import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 function TermekImage() {
   const [sParam] = useSearchParams();
@@ -17,19 +18,31 @@ function TermekImage() {
     setFile(target.files[0]);
   }
 
-  const onSubmit = () => {};
+  const onSubmit = async () => {
+    if (typeof file === "undefined") return;
+    const formData = new FormData();
+    fileName = formData.get("upload-file")!;
+    formData.append("file", file);
+    if (cikkszam != null) {
+      formData.append("cikkszam", cikkszam);
+    }
+    await fetch("http://localhost:8080/uploadImage", {
+      method: "POST",
+      body: formData,
+    });
+    window.location.reload();
+  };
 
   return (
     <>
       <div className="termek-details-img">
         <GetImage ck={cikkszam} />
         <br />
-        <form action="">
-          <input type="file" name="image" onChange={handleOnChange} />
-          <Button type="submit" onClick={onSubmit}>
-            Kép Módosítása
-          </Button>
-        </form>
+
+        <input type="file" name="image" onChange={handleOnChange} />
+        <Button type="submit" onClick={onSubmit}>
+          Kép Módosítása
+        </Button>
       </div>
     </>
   );
