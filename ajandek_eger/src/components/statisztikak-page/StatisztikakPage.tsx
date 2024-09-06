4c3cbbf1597d285e679f1b83d31f04223b78e1e6
Kristof Ulicsny <ulicsny.kristof@gmail.com>
2024-09-06 11:53:54 +0200

    line chart added

--- ajandek_eger/src/components/statisztikak-page/StatisztikakPage.tsx
+++ ajandek_eger/src/components/statisztikak-page/StatisztikakPage.tsx
@@ -1,13 +1,14 @@
 import { useEffect, useState } from "react";
 import TermekTypePie from "./charts/TermekTypePie";
 import axios from "axios";
+import TermekFogyasLine from "./charts/TermekFogyasLine";
 
 function StatisztikakPage() {
   const [termekek, setTermekek] = useState<any[]>([]);
   useEffect(() => {
     const fetchAllTermek = async () => {
       try {
-        const res = await axios.get("http://localhost:8080/all");
+        const res = await axios.get("http://localhost:8080/chartTermekTipus");
         setTermekek(res.data);
       } catch (err) {
         console.log(err);
@@ -16,46 +17,171 @@ function StatisztikakPage() {
     fetchAllTermek();
   }, []);
 
-  const data = [
+  const data2 = [
     {
-      id: "Kiakó Játék",
-      label: "Kiakó Játék",
-      value: 361,
+      id: "us",
+      color: "hsl(289, 70%, 50%)",
+      data: [
+        {
+          x: "plane",
+          y: 115,
+        },
+        {
+          x: "helicopter",
+          y: 22,
+        },
+        {
+          x: "boat",
+          y: 144,
+        },
+        {
+          x: "train",
+          y: 64,
+        },
+        {
+          x: "subway",
+          y: 114,
+        },
+        {
+          x: "bus",
+          y: 104,
+        },
+        {
+          x: "car",
+          y: 199,
+        },
+        {
+          x: "moto",
+          y: 147,
+        },
+        {
+          x: "bicycle",
+          y: 2,
+        },
+        {
+          x: "horse",
+          y: 97,
+        },
+        {
+          x: "skateboard",
+          y: 268,
+        },
+        {
+          x: "others",
+          y: 132,
+        },
+      ],
     },
     {
-      id: "Játék Hangszer",
-      label: "Játék Hangszer",
-      value: 210,
+      id: "germany",
+      color: "hsl(123, 70%, 50%)",
+      data: [
+        {
+          x: "plane",
+          y: 95,
+        },
+        {
+          x: "helicopter",
+          y: 258,
+        },
+        {
+          x: "boat",
+          y: 116,
+        },
+        {
+          x: "train",
+          y: 150,
+        },
+        {
+          x: "subway",
+          y: 178,
+        },
+        {
+          x: "bus",
+          y: 255,
+        },
+        {
+          x: "car",
+          y: 180,
+        },
+        {
+          x: "moto",
+          y: 280,
+        },
+        {
+          x: "bicycle",
+          y: 154,
+        },
+        {
+          x: "horse",
+          y: 182,
+        },
+        {
+          x: "skateboard",
+          y: 215,
+        },
+        {
+          x: "others",
+          y: 277,
+        },
+      ],
     },
     {
-      id: "Póló",
-      label: "Póló",
-      value: 599,
-    },
-    {
-      id: "Pülss",
-      label: "Plüss",
-      value: 82,
-    },
-    {
-      id: "Hűtőmágnes",
-      label: "Hűtőmágnes",
-      value: 381,
+      id: "norway",
+      color: "hsl(100, 70%, 50%)",
+      data: [
+        {
+          x: "plane",
+          y: 131,
+        },
+        {
+          x: "helicopter",
+          y: 4,
+        },
+        {
+          x: "boat",
+          y: 281,
+        },
+        {
+          x: "train",
+          y: 6,
+        },
+        {
+          x: "subway",
+          y: 62,
+        },
+        {
+          x: "bus",
+          y: 222,
+        },
+        {
+          x: "car",
+          y: 119,
+        },
+        {
+          x: "moto",
+          y: 23,
+        },
+        {
+          x: "bicycle",
+          y: 54,
+        },
+        {
+          x: "horse",
+          y: 16,
+        },
+        {
+          x: "skateboard",
+          y: 295,
+        },
+        {
+          x: "others",
+          y: 153,
+        },
+      ],
     },
   ];
 
-  let chartData: any = [];
-  type temp = { id: any; label: any; value: any };
-  const [temp] = useState<temp>({ id: "", label: "", value: "" });
-
-  termekek.map((t) => {
-    temp.id = t.nev;
-    temp.label = t.nev;
-    temp.value = t.db;
-    console.log(temp);
-    chartData.push(temp);
-  });
-  console.log(chartData);
   const datal = termekek.length;
 
   return (
@@ -64,7 +190,7 @@ function StatisztikakPage() {
         <div className="stat-in-div">
           <h3 className="stat-in-title">Termék Típusok</h3>
           <p>Össztermék szám: {datal}</p>
-          <TermekTypePie data={data} />
+          <TermekTypePie data={termekek} />
         </div>
         <div className="stat-in-div">
           <h3 className="stat-in-title">chart2</h3>
@@ -75,7 +201,8 @@ function StatisztikakPage() {
       </div>
       <div className="stat-bottom-div">
         <div className="stat-bottom-in-div">
-          <h3 className="stat-in-title">chart4</h3>
+          <h3 className="stat-in-title">Termék Fogyás</h3>
+          <TermekFogyasLine data={data2} />
         </div>
       </div>
     </>
